194f7013e365d1323ba7ee0b89a4e0e26ff5b912
suesanjp <a.drop.of.my.essence@gmail.com>
2022-05-15 13:59:08 +0900

    changed css styles

--- jstools/tailwind.config.js
+++ jstools/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["../templates/*.{html,js}", "../templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "header-image": "url('/static/img/home-bg.jpg')",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
